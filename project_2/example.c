5d131a4d58aa821405e80f699eb7f459ac119c62
Michael Thornbrugh <mpthornbrugh@gmail.com>
2015-11-30 09:56:48 -0600

    Updated example.c

--- project_2/example.c
+++ project_2/example.c
@@ -1,11 +1,6 @@
- double m[6];
-
-scale(double x) {
-  int i;
-  
-  if (x == 0)
-    return 0;
-  for (i = 0; i < 6; i += 1)
-    m[i] *= x;
-  return 1;
+main(int a, int b)
+{
+	double d;
+	int i;
+	printf("%d %f %d %d\n", i, d, a, b);
 }
